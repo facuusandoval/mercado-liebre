33c11fa7336c2178a5acaf203c4ed286cd7c4707
Facundo Sandoval <facundosandoval2004@gmail.com>
2022-06-21 20:29:54 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+
+const app = express();
+const path = require ('path');
+const PORT = process.env.PORT ||4000;
+const HOST= process.env.HOST || 'localhost';
+
+app.use (express.static(path.resolve(__dirname, './public')));
+
+app. get ('/', (req, res) => {;
+    res.sendFile (path.resolve(__dirname, './views/home.html'))
+});
+
+
+
+app.listen (PORT, () => {
+    console.log ("listening on http://localhost:4000")
+});
